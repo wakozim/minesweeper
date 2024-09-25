d9a02bc984bfc772b92fbb5e5f7aa212c8be6e14
Lookins01 <lookins1q@gmail.com>
2024-09-25 18:39:53 +0500

    [ops] Move source code to separate directory

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ LIBS="-lm -lglfw -ldl -lpthread -lGL -lrt -lX11"
 
 mkdir -p ./build/
 
-clang $CFLAGS -o ./build/minesweeper main.c -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -no-pie -D_DEFAULT_SOURCE $LIBS
+clang $CFLAGS -o ./build/minesweeper ./src/main.c -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -no-pie -D_DEFAULT_SOURCE $LIBS
 
-x86_64-w64-mingw32-gcc -DPLATFORM_DESKTOP -mwindows -Wall -Wextra -ggdb -I./raylib/raylib-5.0_win64_mingw-w64/include/ $CFLAGS -o ./build/minesweeper.exe main.c -L./raylib/raylib-5.0_win64_mingw-w64/lib -l:libraylib.a -lwinmm -lgdi32 -static
+x86_64-w64-mingw32-gcc -DPLATFORM_DESKTOP -mwindows -Wall -Wextra -ggdb -I./raylib/raylib-5.0_win64_mingw-w64/include/ $CFLAGS -o ./build/minesweeper.exe ./src/main.c -L./raylib/raylib-5.0_win64_mingw-w64/lib -l:libraylib.a -lwinmm -lgdi32 -static
