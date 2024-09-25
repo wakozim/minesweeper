6c35ddf74e31b3dfb92e92540a4a436074545f71
Lookins01 <lookins1q@gmail.com>
2024-09-25 18:36:28 +0500

    [build] static link raylib

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 
 set -xe
 
-CFLAGS="-O3 -Wall -Wextra -ggdb -I./thirdparty/ -I. `pkg-config --cflags raylib`"
-LIBS="-lm `pkg-config --libs raylib` -lglfw -ldl -lpthread"
+CFLAGS="-O3 -std=c99 -Wall -Wextra -pedantic -ggdb -I."
+LIBS="-lm -lglfw -ldl -lpthread -lGL -lrt -lX11"
 
 mkdir -p ./build/
-clang $CFLAGS -o ./build/minesweeper main.c $LIBS
+
+clang $CFLAGS -o ./build/minesweeper main.c -L./raylib/raylib-5.0_linux_amd64/lib/ -l:libraylib.a -no-pie -D_DEFAULT_SOURCE $LIBS
+
+x86_64-w64-mingw32-gcc -DPLATFORM_DESKTOP -mwindows -Wall -Wextra -ggdb -I./raylib/raylib-5.0_win64_mingw-w64/include/ $CFLAGS -o ./build/minesweeper.exe main.c -L./raylib/raylib-5.0_win64_mingw-w64/lib -l:libraylib.a -lwinmm -lgdi32 -static
